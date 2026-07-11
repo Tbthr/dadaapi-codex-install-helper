b741927b5988384a80850079301cce135bd8fc50
Ray7086 <1064310743@qq.com>
2026-07-11 18:04:34 +0800

    Register Pinia before mounting the desktop UI

--- apps/desktop/src/main.ts
+++ apps/desktop/src/main.ts
@@ -1,7 +1,10 @@
 import "@fontsource-variable/instrument-sans";
 import "@fontsource-variable/source-serif-4";
+import { createPinia } from "pinia";
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./styles/app.css";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+app.use(createPinia());
+app.mount("#app");
